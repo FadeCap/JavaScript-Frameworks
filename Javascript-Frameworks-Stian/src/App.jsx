f8b5448c3519d946ee1bb23353f911891e8adcc8
FadeCap <stian94lb@gmail.com>
2024-04-30 19:06:39 +0200

    Header + nav + pages update

--- Javascript-Frameworks-Stian/src/App.jsx
+++ Javascript-Frameworks-Stian/src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductPage from "./pages/ProductPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import CartPage from "./pages/CartPage";
+import HomePage from "./pages/HomePage";
+import { Route, Routes} from "react-router-dom"
+import "./App.css";
 
 function App() {
 
+  
+
   return (
-    <Navbar/>
-  )
+    <>
+    <Navbar />
+    <div className="pageContainer">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Checkout" element={<CheckoutPage />} />
+      <Route path="/Cart" element={<CartPage />} />
+      <Route path="/Product" element={<ProductPage />} />
+    </Routes>
+    </div>
+    </>
+  ) 
+    
 }
 
-export default App
+export default App;
--- Javascript-Frameworks-Stian/src/components/Navbar.jsx
+++ Javascript-Frameworks-Stian/src/components/Navbar.jsx
@@ -1,14 +1,28 @@
-export default function Navbar () {
-    return (<nav className="nav d-flex">
-        <a href="/" className="site-title">Site Name</a>
-        <ul>
-            <li>
-                <a href="/CheckoutPage">Checkout</a>
-            </li>
-            <li>
-                <a href="/CartPage">Cart</a>
-            </li>
-        </ul>
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <nav className="nav flex justify-between gap-2 px-2">
+      <Link to="/" className="site-title">
+        Site Name
+      </Link>
+      <ul className="navLinks flex p-0 m-0 gap-4 list-none">
+        <CustomLink to="/Checkout">Checkout</CustomLink>
+        <CustomLink to="/Cart">Cart</CustomLink>
+      </ul>
     </nav>
-    )
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
