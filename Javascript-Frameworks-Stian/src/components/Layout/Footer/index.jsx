6cf481c10ef4f986376645efbbb28a58940ebd68
FadeCap <stian94lb@gmail.com>
2024-05-30 16:39:13 +0200

    Updated footer

--- Javascript-Frameworks-Stian/src/components/Layout/Footer/index.jsx
+++ Javascript-Frameworks-Stian/src/components/Layout/Footer/index.jsx
@@ -5,7 +5,7 @@ import CustomLink from "../../CustomLink";
 export default function Footer() {
     return(
         
-<footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow flex items-center justify-between p-4 bg-gray-800 border-gray-600">
+<footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-dark border-t border-gray-200 shadow flex items-center justify-between p-4 bg-gray-800 border-gray-600">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">FadeCap</Link>. All Rights Reserved.
     </span>
         <li className="flex">
